825adb5b90f5bef514ae0621cfb45b13e35d33a5
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-08-11 17:15:05 +1000

    feat: gnome loadouts & abilities

---
+++ Content.Shared/_Tech/Clothing/ClothingEquipmentRequirementsComponent.cs
@@ -0,0 +1,11 @@
+﻿using Content.Shared.Whitelist;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Tech.Clothing;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ClothingEquipmentRequirementsComponent : Component
+{
+    [DataField]
+    public EntityWhitelist? Whitelist;
+}
---
+++ Content.Shared/_Tech/Clothing/ClothingEquipmentRequirementsSystem.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared.Inventory.Events;
+using Content.Shared.Whitelist;
+
+namespace Content.Shared._Tech.Clothing;
+
+public sealed class ClothingEquipmentRequirementsSystem : EntitySystem
+{
+    [Dependency] private readonly EntityWhitelistSystem _whitelist = null!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ClothingEquipmentRequirementsComponent, BeingEquippedAttemptEvent>(OnAttempt);
+    }
+
+    private void OnAttempt(Entity<ClothingEquipmentRequirementsComponent> entity, ref BeingEquippedAttemptEvent args)
+    {
+        if (_whitelist.IsWhitelistPassOrNull(entity.Comp.Whitelist, args.EquipTarget))
+            return;
+
+        args.Cancel();
+    }
+}
