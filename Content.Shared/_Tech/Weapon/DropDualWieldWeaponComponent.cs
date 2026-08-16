28165c0ab5662f1d43cddabd0bb31925b61c7515
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-08-16 16:19:05 +1000

    feat: drop dual wield weapon

---
+++ Content.Shared/_Tech/Weapon/DropDualWieldWeaponComponent.cs
@@ -0,0 +1,6 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._Tech.Weapon;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class DropDualWieldWeaponComponent : Component;
---
+++ Content.Shared/_Tech/Weapon/DropDualWieldWeaponSystem.cs
@@ -0,0 +1,30 @@
+﻿using Content.Shared._Shitmed.Weapons.Ranged.Events;
+using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Throwing;
+using Content.Shared.Weapons.Ranged.Components;
+using Robust.Shared.Random;
+
+namespace Content.Shared._Tech.Weapon;
+
+public sealed class DropDualWieldWeaponSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _random = null!;
+    [Dependency] private readonly SharedHandsSystem _hands = null!;
+    [Dependency] private readonly ThrowingSystem _throwing = null!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<DropDualWieldWeaponComponent, GunShotBodyEvent>(OnShot);
+    }
+
+    private void OnShot(Entity<DropDualWieldWeaponComponent> entity, ref GunShotBodyEvent args)
+    {
+        if (!HasComp<GunRequiresWieldComponent>(args.GunUid))
+            return;
+
+        _hands.TryDrop(entity.Owner, Transform(entity).Coordinates);
+
+        var direction = _random.NextAngle().ToVec();
+        _throwing.TryThrow(args.GunUid, direction);
+    }
+}
